90ff2df8bee58f38cb4c47496c95ba21dc9cb3a5
Arief Yusron <ariefyusron98@gmail.com>
2018-12-20 05:03:04 +0700

    fix register and login

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -3,37 +3,43 @@ const User = use('App/Models/User')
 
 class AuthController {
     async register({request, auth, response}) {
-        const name = request.input("name")
-        const username = request.input("username")
-        const email = request.input('email')
-        const password = request.input("password")
+        const register = request.only([
+            'name',
+            'username',
+            'email',
+            'password'
+        ])
 
-        let user = new User()
-        user.name = name
-        user.username = username
-        user.email = email
-        user.password = password
+        const user = new User()
+        user.name = register.name
+        user.username = register.username
+        user.email = register.email
+        user.password = register.password
 
-        user = await user.save()
+        await user.save()
 
-        let accessToken = await auth.generate(user)
-        return response.json({"user": user, "access_token": accessToken})
+        const accessToken = await auth.withRefreshToken().generate(user)
+        return response.json({
+            'user': user,
+            'access_token': accessToken
+        })
     }
 
     async login({request, auth, response}) {
-        const username = request.input("username")
-        const password = request.input("password")
+        const login = request.only([
+            'email',
+            'password'
+        ])
+        const email = login.email
+        const password = login.password
         
-        try {
-            if (await auth.attemp(username, password)) {
-                let user = await User.findBy('username', username)
-                let accessToken = await auth.generate(user)
-                return response.json({"user": user, "access_token": accessToken})
-            }
-        } catch (error) {
-            return response.json({message: 'You first need to register!'})
-            
-        }
+        const user = await User.findBy('email',email)
+        const accessToken = await auth.withRefreshToken().attempt(email,password)
+        
+        return response.json({
+            'user': user,
+            'access_token': accessToken
+        })
     }
 }
 
